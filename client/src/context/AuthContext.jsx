52f2b39cf29820a2203eb757c3e1d4f4722d7992
umtt31 <umutcan.adiguzel@outlook.com>
2024-01-31 22:39:54 +0300

    context API

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import { useCallback } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, registerInfo, updateRegisterInfo }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
