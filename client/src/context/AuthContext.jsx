e4556f42db875a75d91141d0b56fa1a8190ba552
umtt31 <umutcan.adiguzel@outlook.com>
2024-01-31 23:11:09 +0300

    Register user with fetch

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react";
 import { useCallback } from "react";
+import { baseUrl, postRequest } from "../utils/services";
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [registerError, setRegisterError] = useState(null);
+  const [isRegisterLoading, setIsRegisterLoading] = useState(false);
   const [registerInfo, setRegisterInfo] = useState({
     name: "",
     email: "",
@@ -15,8 +18,38 @@ export const AuthContextProvider = ({ children }) => {
     setRegisterInfo(info);
   }, []);
 
+  const registerUser = useCallback(async (e) => {
+    e.preventDefault();
+
+    setIsRegisterLoading(true);
+    setRegisterError(null);
+
+    const response = await postRequest(
+      `${baseUrl}/users/register`,
+      JSON.stringify(registerInfo)
+    );
+
+    setIsRegisterLoading(false);
+
+    if (response?.error) {
+      return setRegisterError(response);
+    }
+
+    localStorage.setItem("User", JSON.stringify(response));
+    setUser(response);
+  }, [registerInfo]);
+
   return (
-    <AuthContext.Provider value={{ user, registerInfo, updateRegisterInfo }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        registerInfo,
+        updateRegisterInfo,
+        registerUser,
+        registerError,
+        isRegisterLoading,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,10 +3,11 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
-  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
+  const { registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading } =
+    useContext(AuthContext);
   return (
     <>
-      <Form>
+      <Form onSubmit={registerUser}>
         <Row
           style={{
             height: "100vh",
@@ -18,16 +19,39 @@ const Register = () => {
             <Stack gap={3}>
               <h2>Register</h2>
 
-              <Form.Control type="text" placeholder="Name" onChange={e => updateRegisterInfo({...registerInfo, name: e.target.value})} />
-              <Form.Control type="email" placeholder="Email" onChange={e => updateRegisterInfo({...registerInfo, email: e.target.value})} />
-              <Form.Control type="password" placeholder="Password" onChange={e => updateRegisterInfo({...registerInfo, password: e.target.value})} />
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, name: e.target.value })
+                }
+              />
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  })
+                }
+              />
               <Button variant="primary" type="submit">
-                Register
+                {isRegisterLoading ? "Creating your account.": "Register"}
               </Button>
 
-              <Alert variant="danger">
-                <p>An error occured.</p>
-              </Alert>
+              {registerError?.error && (
+                <Alert variant="danger">
+                  <p>{registerError.error ? registerError.message : ""}</p>
+                </Alert>
+              )}
             </Stack>
           </Col>
         </Row>
